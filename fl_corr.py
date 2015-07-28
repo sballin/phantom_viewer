fix average subtraction

fl_corr.py
@@ -63,8 +63,7 @@ def plot_fl_slider(shot, sav):
     """
     fls = sav.fl_image
     fl_r = sav.fieldline_r; fl_z = sav.fieldline_z
-    subs = acquire.video(shot, 'phantom2', sub=5, sobel=False)
-    frames = subs[:2048]
+    frames = acquire.video(shot, 'phantom2', sub=5, sobel=False)
     gpi_index = 1028
     xcorrs = np.zeros(fls.shape[0])
     for i, fl in enumerate(fls):
@@ -136,11 +135,11 @@ def plot_fl_slider(shot, sav):
         
 
 if __name__ == '__main__':
-    shot = 1150611004
+    shot = 1150717011
     #show_x_pt_fl(shot)
     
     # Get field line projection images
-    sav = readsav('/usr/local/cmod/codes/spectroscopy/ir/FLIR/fl_images_1150611004.sav')
+    sav = readsav('fl_images_%s.sav' % shot)
     fls = sav.fl_image
     #fls_cross_section_plot(sav)
     plot_fl_slider(shot, sav)

frame_corr.py
@@ -5,6 +5,7 @@ import acquire
 import view
 import norm_xcorr
 from tqdm import *
+import scipy.signal
 
 
 def corr_frame(frames, pixel, nofft=False, other_pixels=None, show_maxes=False):
@@ -32,7 +33,7 @@ def corr_frame(frames, pixel, nofft=False, other_pixels=None, show_maxes=False):
         plt.show()
     
         plt.figure(); plt.title('Maximum correlations')
-        plt.imshow(max_lags, origin='bottom'); plt.colorbar(label='Lag (frames)')
+        plt.imshow(max_lags, origin='bottom', vmin=-75, vmax=75); plt.colorbar(label='Lag (frames)')
         plt.show()
 
     view.slide_corr(corr, pixel, other_pixels=other_pixels)
@@ -73,16 +74,27 @@ def corr_plot_custom(frames, pixel, other_pixels):
 
 
 if __name__ == '__main__':
-    shot = 1150611004 #1150528015 
+    shot = 1150722015 #1150611004 #1150528015 
     camera = 'phantom2'
-    frames = acquire.video(shot, camera, sub=5, sobel=False)
+
+    frames = acquire.video(shot, camera, sub=0, sobel=False)
     t_hists = acquire.gpi_series(shot, camera, 't_hists')#[:, :4]
     time = acquire.gpi_series(shot, camera, 'time')
 
-    custom_pixels = [(32, i) for i in reversed(xrange(4, 29, 3))]
-    #corr_plot_custom(frames, (32, 32), custom_pixels)
+    # Pixels along divider
+    #custom_pixels = [(15, 35), (25, 34), (35, 30), (44, 24), (51, 20), (58, 13)] 
+    # Pixels along blob field line
+    #custom_pixels = [(15, 35), (23, 31), (30, 26), (33, 21), (36, 14), (37, 8), (35, 2)]
+    # Pixels descending vertically in middle
+    #custom_pixels = [(32, i) for i in reversed(xrange(4, 29, 3))]
+
+    #corr_plot_custom(frames, (35, 5), custom_pixels)
+
     #corr_plot(t_hists[22500:24325], 1, time, nofft=True)
+
     time_step = (time[-1]-time[0])/len(time)
-    signals.ps_explorer(frames[22500:24325, 14, 10], time_step)
-    #corr_frame(frames[22500:24325], (32, 32), other_pixels=custom_pixels, show_maxes=True)
+    plt.figure(); plt.plot(time, frames[:, 14, 10]); plt.show()
+    signals.ps_explorer(frames[:, 14, 10], time_step)
+
+    #corr_frame(frames[22500:24325], (35, 5), other_pixels=custom_pixels)
  

process.py
@@ -49,13 +49,17 @@ def average_frames(frames, interval):
         NumPy array: repeated averages [a a a... b b b... c c c... d d d...] 
                      for each [interval] frames supplied
     """
-    dim = frames.shape
-    # Average for every [interval] frames, length = frame_count/interval
-    avg_count = dim[0]/interval
-    avg = np.mean(frames.reshape(interval, avg_count, dim[1], dim[2]), axis=0) 
-    # Averages [a b c d] => [a a a... b b b... c c c... d d d...] 
-    return np.array([[avg[i] for j in xrange(interval)] for i in 
-                     xrange(avg_count)]).reshape(dim)
+    frame_count = frames.shape[0]
+    halfint = interval/2
+    averages = np.zeros(frames.shape)
+    for i in range(frame_count):
+        if i < halfint:
+            averages[i] = np.mean(frames[:interval], axis=0)
+        elif frame_count - i < halfint:
+            averages[i] = np.mean(frames[-interval:], axis=0)
+        else:
+            averages[i] = np.mean(frames[i-halfint:i+halfint], axis=0)
+    return averages
 
 
 def subtract_average(frames, interval):

signals.py
@@ -1,37 +1,17 @@
 from __future__ import division
 import numpy as np
 import scipy
+import scipy.signal
 import warnings
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button
 
 
 def ps_explorer(series, time_step):
-    freqs, PS = scipy.signal.welch(series, nperseg=256, detrend='linear', scaling='spectrum', fs=1./time_step)
-    # Initial plotting
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
-    plt.subplots_adjust(bottom=0.25)
-    #p, = plt.plot(freqs, PS)
-    for i in range(7, 13):
+    plt.figure()
+    for i in range(8, 13):
         freqs, PS = scipy.signal.welch(series, nperseg=2**i, detrend='linear', scaling='spectrum', fs=1./time_step)
         plt.plot(freqs, PS)
-    plt.show()
-
-
-    # Slider and button settings
-    slide_area = plt.axes([0.10, 0.1, 0.65, 0.03])
-    slider = Slider(slide_area, 'nperseg', 7, 12, valinit=7)
-    slider.drawon = True
-    slider.valfmt = '2**%d'
-
-    def update(val):
-        freqs, PS = scipy.signal.welch(series, nperseg=2**val, detrend='linear', scaling='spectrum', fs=1./time_step)
-        p.set_xdata(freqs); p.set_ydata(PS)
-        #slider.valtext.set_text('%d' % val)
-        fig.canvas.draw_idle()
-
-    slider.on_changed(update)
     plt.show()
 
 